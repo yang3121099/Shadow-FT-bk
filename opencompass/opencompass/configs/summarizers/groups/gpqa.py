701b23aa5ddf4454d416d016e2319b4aaf0cfaa4
yang <971613262@qq.com>
2025-07-27 13:52:54 +0000

    update 2025-07-27

---
+++ opencompass/opencompass/configs/summarizers/groups/gpqa.py
@@ -0,0 +1,22 @@
+GPQA_summary_groups = []
+
+# bbh
+_GPQA = [ 'diamond', 'main', 'extended']
+_GPQA = ['GPQA_' + s for s in _GPQA]
+GPQA_summary_groups.append({'name': 'GPQA', 'subsets': _GPQA})
+
+
+GPQA1_summary_groups = []
+
+# bbh
+_GPQA1 = [ 'diamond', 'main', 'extended']
+_GPQA1 = ['GPQA1_' + s for s in _GPQA1]
+GPQA1_summary_groups.append({'name': 'GPQA1', 'subsets': _GPQA1})
+
+
+GPQA2_summary_groups = []
+
+# bbh
+_GPQA2 = [ 'diamond', 'main', 'extended']
+_GPQA2 = ['GPQA2_' + s for s in _GPQA2]
+GPQA2_summary_groups.append({'name': 'GPQA2', 'subsets': _GPQA2})
