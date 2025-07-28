1545f7d0bf98b598ab333f4edd118c401e9485a5
yang <971613262@qq.com>
2025-07-28 04:51:40 +0000

    update 2025-07-28

--- opencompass/eval_quantw_20250727-bk.py
+++ opencompass/eval_quantw_20250727-bk.py
@@ -24,7 +24,7 @@ with read_base():
     from opencompass.configs.datasets.ARC_c.ARC_c_cot_gen_926652 import ARC_c_datasets #效果还行，比官方还高    
     from opencompass.configs.datasets.math.math_500_gen import math_datasets as math_500_datasets  # math_500
     from opencompass.configs.datasets.humaneval.humaneval_gen_8e312c import humaneval_datasets
-    from opencompass.configs.datasets.mbpp.sanitized_mbpp_mdblock_gen_a447ff import sanitized_mbpp_datasets
+    # from opencompass.configs.datasets.mbpp.sanitized_mbpp_mdblock_gen_a447ff import sanitized_mbpp_datasets
     
 datasets = sum((v for k, v in locals().items() if k.endswith('_datasets')), [])
 
@@ -32,26 +32,26 @@ datasets = sum((v for k, v in locals().items() if k.endswith('_datasets')), [])
 #                        PART 2  Models  List                         #
 #######################################################################
 
-work_dir = f'outputs/Rebuttal-0727/'
-# work_dir = f'outputs/Rebuttal-quant-0727/'
+# work_dir = f'outputs/Rebuttal-0727/'
+work_dir = f'outputs/Rebuttal-quant-0727/'
 
 from opencompass.models import TurboMindModelwithChatTemplate
 
 # input .sh output "##### Evaluation list #####" below
 Baseline_settings = [
     
-# ('Llama-2-7b-Baseline', 'meta-llama/Llama-2-7b'),
-('Llama-2-7b-E8P-2Bit', 'relaxml/Llama-2-7b-E8P-2Bit'),
-('Llama-2-7b-E8PRVQ-3Bit', 'relaxml/Llama-2-7b-E8PRVQ-3Bit'),
-('Llama-2-7b-E8PRVQ-4Bit', 'relaxml/Llama-2-7b-E8PRVQ-4Bit'),
-('Llama-2-7b-QTIP-4Bit', 'relaxml/Llama-2-7b-QTIP-4Bit'),
-('Llama-2-7b-QTIP-3Bit', 'relaxml/Llama-2-7b-QTIP-3Bit'),
-('Llama-2-7b-QTIP-2Bit', 'relaxml/Llama-2-7b-QTIP-2Bit'),
-('Llama-2-7B-Hessians-2Sided', 'relaxml/Llama-2-7B-Hessians-2Sided'),
-('Llama-2-7b-AQLM-2Bit-1x16-hf','ISTA-DASLab/Llama-2-7b-AQLM-2Bit-1x16-hf'),
-('Llama-2-7b-AQLM-PV-2Bit-1x16-hf','ISTA-DASLab/Llama-2-7b-AQLM-PV-2Bit-1x16-hf'),
-('Llama-2-7b-AQLM-PV-2Bit-1x16-hf',']ISTA-DASLab/Llama-2-7b-AQLM-PV-2Bit-1x16-hf'),
-('Llama-2-7b-AQLM-PV-1Bit-1x16-hf','ISTA-DASLab/Llama-2-7b-AQLM-PV-1Bit-1x16-hf'),
+('Llama-2-7b-Baseline', 'meta-llama/Llama-2-7b'),
+# ('Llama-2-7b-E8P-2Bit', 'relaxml/Llama-2-7b-E8P-2Bit'),
+# ('Llama-2-7b-E8PRVQ-3Bit', 'relaxml/Llama-2-7b-E8PRVQ-3Bit'),
+# ('Llama-2-7b-E8PRVQ-4Bit', 'relaxml/Llama-2-7b-E8PRVQ-4Bit'),
+# ('Llama-2-7b-QTIP-4Bit', 'relaxml/Llama-2-7b-QTIP-4Bit'),
+# ('Llama-2-7b-QTIP-3Bit', 'relaxml/Llama-2-7b-QTIP-3Bit'),
+# ('Llama-2-7b-QTIP-2Bit', 'relaxml/Llama-2-7b-QTIP-2Bit'),
+# ('Llama-2-7B-Hessians-2Sided', 'relaxml/Llama-2-7B-Hessians-2Sided'),
+# ('Llama-2-7b-AQLM-2Bit-1x16-hf','ISTA-DASLab/Llama-2-7b-AQLM-2Bit-1x16-hf'),
+# ('Llama-2-7b-AQLM-PV-2Bit-1x16-hf','ISTA-DASLab/Llama-2-7b-AQLM-PV-2Bit-1x16-hf'),
+# ('Llama-2-7b-AQLM-PV-2Bit-1x16-hf',']ISTA-DASLab/Llama-2-7b-AQLM-PV-2Bit-1x16-hf'),
+# ('Llama-2-7b-AQLM-PV-1Bit-1x16-hf','ISTA-DASLab/Llama-2-7b-AQLM-PV-1Bit-1x16-hf'),
 # ('',''),
 # ('Qwen3-0.6B-Instruct-hf', 'Qwen/Qwen3-0.6B'),
 # ('Qwen3-4B-Instruct-hf', 'Qwen/Qwen3-4B'),
@@ -78,8 +78,8 @@ for abbr, path in Baseline_settings:  ## classic 4096
             abbr=abbr,
             path=path,
             max_out_len=1024,
-            batch_size=512,
-            run_cfg=dict(num_gpus=8),
+            batch_size=128,
+            run_cfg=dict(num_gpus=1),
         )
     ]
 
